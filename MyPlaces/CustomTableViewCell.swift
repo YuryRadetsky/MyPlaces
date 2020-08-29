2db8175ee95a285718f090245f32641ec3566102
Yury Radetsky <yura.radetsky@gmail.com>
2020-08-29 22:54:17 +0300

    Custom table view cell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Yury Radetsky on 29.08.2020.
+//  Copyright © 2020 YuryRadetsky. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+
+
+}
