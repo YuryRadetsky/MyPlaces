1ac7fcf75f51b177b2f12e0668fd5f1073517f1e
Yury Radetsky <yura.radetsky@gmail.com>
2020-09-06 19:58:56 +0300

    Realm

--- MyPlaces/MainViewController.swift
+++ MyPlaces/MainViewController.swift
@@ -10,8 +10,7 @@ import UIKit
 
 class MainViewController: UITableViewController {
     
-
-    let places = Place.getPlaces()
+//    var places = Place.getPlaces()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,25 +19,33 @@ class MainViewController: UITableViewController {
     
     // MARK: - Table view data source
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return restaurantNames.count
-        return places.count
-    }
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+////        return restaurantNames.count
+//        return places.count
+//    }
+//
     
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
-        
-        cell.nameLabel.text = places[indexPath.row].name
-        cell.locationLabel.text = places[indexPath.row].location
-        cell.typeLabel.text = places[indexPath.row].type
-        cell.imageOfPlace.image = UIImage(named: places[indexPath.row].image)
-        
-        cell.imageOfPlace.layer.cornerRadius = cell.imageOfPlace.frame.size.height / 2
-        cell.imageOfPlace.clipsToBounds = true
-        
-        return cell
-    }
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
+//
+//        let place = places[indexPath.row]
+//
+//        cell.nameLabel.text = place.name
+//        cell.locationLabel.text = place.location
+//        cell.typeLabel.text = place.type
+//
+//        if place.image == nil {
+//            cell.imageOfPlace.image = UIImage(named: place.restaurantImage!)
+//        } else {
+//            cell.imageOfPlace.image  = place.image
+//        }
+//
+//
+//        cell.imageOfPlace.layer.cornerRadius = cell.imageOfPlace.frame.size.height / 2
+//        cell.imageOfPlace.clipsToBounds = true
+//
+//        return cell
+//    }
     
     // MARK: - Table view delegate
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -50,9 +57,19 @@ class MainViewController: UITableViewController {
      
      // In a storyboard-based application, you will often want to do a little preparation before navigation
      override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
+     // Get the new view contr oller using segue.destination.
      // Pass the selected object to the new view controller.
      }
      */
-    @IBAction func cancelAction (_ seqgue: UIStoryboardSegue) {}
+    
+    @IBAction func unwindSeque (_ seqgue: UIStoryboardSegue) {
+        
+        guard let newPlaceVC = seqgue.source as? NewPlaceViewController else { return }
+        
+        newPlaceVC.saveNewPlace()
+//        places.append(newPlaceVC.newPlace!)
+        tableView.reloadData()
+        
+        
+    }
 }
--- MyPlaces/NewPlaceViewController.swift
+++ MyPlaces/NewPlaceViewController.swift
@@ -9,16 +9,33 @@
 import UIKit
 
 class NewPlaceViewController: UITableViewController {
-    @IBOutlet weak var imageOfPlace: UIImageView!
+    
+    var newPlace = Place()
+    var imageIsChanged = false
+    
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    
+    @IBOutlet weak var placeImage: UIImageView!
+    @IBOutlet weak var placeName: UITextField!
+    @IBOutlet weak var placeLocation: UITextField!
+    @IBOutlet weak var placeType: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
+        
+        DispatchQueue.main.async {
+            self.newPlace.savePlaces()
+        }
+        
         tableView.tableFooterView = UIView()
+        
+        saveButton.isEnabled = false
+        
+        placeName.addTarget(self, action: #selector(textFieldChanged), for: .editingChanged)
+        
     }
-
-  // MARK: - Table view delegate
+    
+    // MARK: - Table view delegate
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.row == 0 {
@@ -56,7 +73,28 @@ class NewPlaceViewController: UITableViewController {
             view.endEditing(true)
         }
     }
-
+    
+    func saveNewPlace() {
+        
+        var image: UIImage?
+        
+        if imageIsChanged {
+            image = placeImage.image
+        } else {
+            image = #imageLiteral(resourceName: "imagePlaceholder")
+        }
+        
+//        newPlace = Place(name: placeName.text!,
+//                         location: placeLocation.text,
+//                         type: placeType.text,
+//                         image: image,
+//                         restaurantImage: nil)
+    }
+    
+    @IBAction func cancelAction(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
 
 
@@ -67,6 +105,14 @@ extension NewPlaceViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
+    }
+    
+    @objc private func textFieldChanged() {
+        if placeName.text?.isEmpty == false {
+            saveButton.isEnabled = true
+        } else {
+            saveButton.isEnabled = false
+        }
     }
     
 }
@@ -86,9 +132,13 @@ extension NewPlaceViewController: UIImagePickerControllerDelegate, UINavigationC
     
     func imagePickerController(_ picker: UIImagePickerController,
                                didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        imageOfPlace.image = info[.editedImage] as? UIImage
-        imageOfPlace.contentMode = .scaleAspectFill
-        imageOfPlace.clipsToBounds = true
+        
+        placeImage.image = info[.editedImage] as? UIImage
+        placeImage.contentMode = .scaleAspectFill
+        placeImage.clipsToBounds = true
+        
+        imageIsChanged = true
+        
         dismiss(animated: true, completion: nil)
     }
 }
