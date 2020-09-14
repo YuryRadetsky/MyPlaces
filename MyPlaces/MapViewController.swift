5e496214a598dd7f91865fbc90f860f254f71ea0
Yury Radetsky <yura.radetsky@gmail.com>
2020-09-14 18:26:46 +0300

    Map View Controller

---
+++ MyPlaces/MapViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MapViewController.swift
+//  MyPlaces
+//
+//  Created by Yury Radetsky on 14.09.2020.
+//  Copyright © 2020 YuryRadetsky. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    @IBAction func closeVC() {
+        dismiss(animated: true)
+    }
+    
+}
